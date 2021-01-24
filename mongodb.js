fd536f619beda2808aca484114a604f92d28dc46
Sayaka Ono <sayaka.ono.81@gmail.com>
2021-01-24 08:39:45 -0800

    database operation examples

--- mongodb.js
+++ mongodb.js
@@ -1,5 +1,9 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+const id = new ObjectID();
+console.log(id.id);
+console.log(id.getTimestamp());
+console.log(id.toHexString().length);
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
@@ -14,9 +18,43 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-      name: 'Sayaka',
-      age: 32
-    });
+    db.collection('users').insertOne(
+      {
+        name: 'Vikram',
+        age: 32
+      },
+      (error, result) => {
+        if (error) {
+          return console.log('Unable to insert user');
+        }
+
+        console.log(result.ops);
+      }
+    );
+
+    db.collection('users').insertMany(
+      [{ name: 'Jen', age: 28 }, { name: 'Gunteher', age: 27 }],
+      (error, result) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log(result.ops);
+      }
+    );
+
+    db.collection('tasks').insertMany(
+      [
+        { description: 'cleaning', completed: false },
+        { description: 'cooking', completed: false },
+        { description: 'studyin', completed: true }
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log('Unable to insert tasks!');
+        }
+
+        console.log(result.ops);
+      }
+    );
   }
 );
