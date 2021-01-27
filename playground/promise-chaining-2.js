0e4115583df0d631721fad3f2c67628f6ba10f25
Sayaka Ono <sayaka.ono.81@gmail.com>
2021-01-27 08:18:25 -0800

    add Async/Await examples

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,10 +1,20 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('600dc1defe32e3176f3f63b3')
-  .then(task => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-  })
+// Task.findByIdAndDelete('600dc1defe32e3176f3f63b3')
+//   .then(task => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then(result => console.log(result))
+//   .catch(e => console.log(e));
+
+const deleteTaskAndCount = async id => {
+  const task = await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: false });
+  return count;
+};
+
+deleteTaskAndCount('600dcbfb1c135a731d4d26be')
   .then(result => console.log(result))
   .catch(e => console.log(e));
