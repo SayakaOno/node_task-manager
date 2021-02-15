8495093234caa13db55cfe8546b10f9dd214b596
Sayaka Ono <sayaka.ono.81@gmail.com>
2021-02-15 12:04:53 -0800

    associate avatar image with user, add delete avatar endpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,18 +3,6 @@ const multer = require('multer');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const router = new express.Router();
-const upload = multer({
-  dest: 'avatars',
-  limits: {
-    fileSize: 1000000
-  },
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-      return cb(new Error('Please upload an image'));
-    }
-    cb(undefined, true);
-  }
-});
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
@@ -111,15 +99,36 @@ router.delete('/users/me', auth, async (req, res) => {
   }
 });
 
+const upload = multer({
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload an image'));
+    }
+    cb(undefined, true);
+  }
+});
+
 router.post(
   '/users/me/avatar',
+  auth,
   upload.single('avatar'),
-  (req, res) => {
+  async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
     res.send();
   },
   (error, req, res, next) => {
     res.status(400).send({ error: error.message });
   }
 );
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+  res.send();
+});
 
 module.exports = router;
