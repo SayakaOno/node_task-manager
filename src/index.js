5184e0d4c1093e9b6999d472fd29a2480cefb394
Sayaka Ono <sayaka.ono.81@gmail.com>
2021-02-15 10:41:18 -0800

    add upload endpoint

--- src/index.js
+++ src/index.js
@@ -6,17 +6,34 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//   if (req.method === 'GET') {
-//     res.send('GET requests are disabled');
-//   } else {
-//     next();
-//   }
-// });
+const multer = require('multer');
+const upload = multer({
+  dest: 'images',
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error('Please upload a Word document'));
+    }
 
-// app.use((req, res, next) => {
-//   res.status(503).send('Under maintenance');
-// });
+    cb(undefined, true);
+    // cb(new Error('File must be a PDF'))
+    // cb(undefined, true)
+    // cb(undefined, false)
+  }
+});
+
+app.post(
+  '/upload',
+  upload.single('upload'),
+  (req, res) => {
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
 app.use(express.json());
 app.use(userRouter);
@@ -25,18 +42,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
 });
-
-const Task = require('./models/task');
-const User = require('./models/user');
-
-const main = async () => {
-  // const task = await Task.findById('601f975203b6a64ad788af8d');
-  // await task.populate('owner').execPopulate()
-  // console.log(task.owner);
-
-  const user = await User.findById('601f970f03b6a64ad788af8b');
-  await user.populate('tasks').execPopulate();
-  console.log(user.tasks);
-};
-
-main();
